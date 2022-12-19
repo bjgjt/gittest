e8a50c83ffc989f7f96ba1a801af6c333230d8a9
tuanln <tuanln@vnpt.vn>
2022-12-19 09:50:31 +0700

    zxc

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hello World!");
+
+console.log("main");
+
+console.log("Hello my friend!");
+
+console.log("main2");
+console.log("test1");
+
+console.log("Hello my friend!");
+
+console.log("test2");
